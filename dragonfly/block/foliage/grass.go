76cfe5dbe467a1ca3a1587b6180c02559347c3c5
Civiled <civiled@usa.com>
2020-12-22 21:01:18 -0800

    Added tall_grass base

---
+++ dragonfly/block/foliage/grass.go
@@ -0,0 +1,17 @@
+package foliage
+
+type DoubleTallGrass string
+type SingleTallGrass string
+
+const (
+	TallGrass DoubleTallGrass = "grass"
+	TallFern  DoubleTallGrass = "fern"
+	Paeonia   DoubleTallGrass = "paeonia"
+	Rose      DoubleTallGrass = "rose"
+	Sunflower DoubleTallGrass = "sunflower"
+	Syringa   DoubleTallGrass = "syringa"
+
+	Grass SingleTallGrass = "tall"
+	Fern  SingleTallGrass = "fern"
+	Snow  SingleTallGrass = "snow"
+)
---
+++ dragonfly/block/tall_grass.go
@@ -0,0 +1,7 @@
+package block
+
+import "github.com/df-mc/dragonfly/dragonfly/block/foliage"
+
+type TallGrass struct {
+	GrassType foliage.SingleTallGrass
+}
